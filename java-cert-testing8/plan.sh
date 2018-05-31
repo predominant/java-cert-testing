dd16a172fd1f784dc8f6c77006955d6cc14296a4
Graham Weldon <graham@grahamweldon.com>
2018-05-31 10:49:47 +0900

    Stop trying to be clever.. just get it done
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- java-cert-testing8/plan.sh
+++ java-cert-testing8/plan.sh
@@ -1,4 +1,17 @@
-. ../java-cert-testing7/plan.sh
 pkg_name=java-cert-testing8
+pkg_origin=predominant
+pkg_version="0.1.0"
+pkg_maintainer="Graham Weldon <graham@grahamweldon.com>"
+pkg_license=("Apache-2.0")
 pkg_deps=(core/jre8)
 pkg_build_deps=(core/jdk8)
+
+do_build() {
+  javac ../CertTester.java
+  jar cfe CertTester.jar CertTester ../CertTester.class
+  rm ../CertTester.class
+}
+
+do_install() {
+  mv CertTester.jar "${pkg_prefix}"/
+}
